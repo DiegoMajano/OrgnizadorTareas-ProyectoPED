2c04ef89ad42e5718db0a6b6fc617dcba12c22b2
wighot <aleartiga12@gmail.com>
2024-05-14 22:11:13 -0600

    .

--- FlatLogin/FormLogin.cs
+++ FlatLogin/FormLogin.cs
@@ -101,6 +101,7 @@ namespace FlatLoginWatermark
                 // Verificar si las credenciales coinciden con las almacenadas
                 if (usuarioIngresado == usuarioValido && contraseñaIngresada == contraseñaValida)
                 {
+                    
                     frmPrincipal formPrincipal = new frmPrincipal();
                     this.Hide();
                     formPrincipal.Show();              
