8c39fe5b00aa920eef552980e53213ccd912861e
WillyUDB <willyantoni147@gmail.com>
2024-05-16 23:31:34 -0600

    Actualizacion de frmTecnicaEstudio

--- NOTES/ModernGUI_V3/frmTecnicaEstudio.Designer.cs
+++ NOTES/ModernGUI_V3/frmTecnicaEstudio.Designer.cs
@@ -31,39 +31,39 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTecnicaEstudio));
             this.panelformularios = new System.Windows.Forms.Panel();
-            this.label3 = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelTimer = new System.Windows.Forms.Label();
+            this.btnEndSession = new System.Windows.Forms.Button();
+            this.btnSkip = new System.Windows.Forms.Button();
+            this.btnStartPause = new System.Windows.Forms.Button();
+            this.textBoxIterations = new System.Windows.Forms.TextBox();
+            this.textBoxStatus = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.lblfecha = new System.Windows.Forms.Label();
             this.lblhora = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.timerPomodoro = new System.Windows.Forms.Timer(this.components);
+            this.timermov = new System.Windows.Forms.Timer(this.components);
             this.panelformularios.SuspendLayout();
-            this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panelformularios
             // 
             this.panelformularios.BackColor = System.Drawing.SystemColors.Control;
-            this.panelformularios.Controls.Add(this.label3);
-            this.panelformularios.Controls.Add(this.panel1);
+            this.panelformularios.Controls.Add(this.pictureBox2);
+            this.panelformularios.Controls.Add(this.pictureBox1);
+            this.panelformularios.Controls.Add(this.labelTimer);
+            this.panelformularios.Controls.Add(this.btnEndSession);
+            this.panelformularios.Controls.Add(this.btnSkip);
+            this.panelformularios.Controls.Add(this.btnStartPause);
+            this.panelformularios.Controls.Add(this.textBoxIterations);
+            this.panelformularios.Controls.Add(this.textBoxStatus);
+            this.panelformularios.Controls.Add(this.label2);
             this.panelformularios.Controls.Add(this.lblfecha);
             this.panelformularios.Controls.Add(this.lblhora);
-            this.panelformularios.Controls.Add(this.label1);
-            this.panelformularios.Controls.Add(this.pictureBox1);
-            this.panelformularios.Controls.Add(this.label2);
-            this.panelformularios.Controls.Add(this.pictureBox2);
             this.panelformularios.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelformularios.Location = new System.Drawing.Point(0, 0);
             this.panelformularios.Margin = new System.Windows.Forms.Padding(2);
@@ -71,77 +71,99 @@
             this.panelformularios.Size = new System.Drawing.Size(777, 523);
             this.panelformularios.TabIndex = 6;
             // 
-            // label3
+            // pictureBox2
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Sitka Text", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(292, 479);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(142, 28);
-            this.label3.TabIndex = 20;
-            this.label3.Text = "Time to Focus!";
+            this.pictureBox2.Location = new System.Drawing.Point(685, 480);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(190, 141);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 28;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
             // 
-            // panel1
+            // pictureBox1
             // 
-            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Controls.Add(this.pictureBox4);
-            this.panel1.Controls.Add(this.button3);
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.pictureBox3);
-            this.panel1.Location = new System.Drawing.Point(100, 188);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(548, 288);
-            this.panel1.TabIndex = 19;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(121, 86);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 27;
+            this.pictureBox1.TabStop = false;
             // 
-            // button3
+            // labelTimer
             // 
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Font = new System.Drawing.Font("Sitka Text", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(346, 13);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(115, 35);
-            this.button3.TabIndex = 1;
-            this.button3.Text = "break";
-            this.button3.UseVisualStyleBackColor = true;
+            this.labelTimer.AutoSize = true;
+            this.labelTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTimer.Location = new System.Drawing.Point(42, 134);
+            this.labelTimer.Name = "labelTimer";
+            this.labelTimer.Size = new System.Drawing.Size(284, 108);
+            this.labelTimer.TabIndex = 26;
+            this.labelTimer.Text = "00:00";
             // 
-            // button2
+            // btnEndSession
             // 
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Sitka Text", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(67, 13);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(113, 35);
-            this.button2.TabIndex = 0;
-            this.button2.Text = "Pomodoro";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnEndSession.Location = new System.Drawing.Point(360, 228);
+            this.btnEndSession.Name = "btnEndSession";
+            this.btnEndSession.Size = new System.Drawing.Size(75, 23);
+            this.btnEndSession.TabIndex = 25;
+            this.btnEndSession.Text = "End";
+            this.btnEndSession.UseVisualStyleBackColor = true;
+            this.btnEndSession.Click += new System.EventHandler(this.btnEndSession_Click);
+            this.btnEndSession.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnEndSession_MouseClick);
             // 
-            // button1
+            // btnSkip
             // 
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(231, 222);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 16;
-            this.button1.Text = "Start";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnSkip.Location = new System.Drawing.Point(360, 170);
+            this.btnSkip.Name = "btnSkip";
+            this.btnSkip.Size = new System.Drawing.Size(75, 23);
+            this.btnSkip.TabIndex = 24;
+            this.btnSkip.Text = "Skip";
+            this.btnSkip.UseVisualStyleBackColor = true;
+            this.btnSkip.Click += new System.EventHandler(this.btnSkip_Click);
             // 
-            // pictureBox3
+            // btnStartPause
             // 
-            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(185, 222);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(29, 23);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox3.TabIndex = 15;
-            this.pictureBox3.TabStop = false;
+            this.btnStartPause.Location = new System.Drawing.Point(360, 111);
+            this.btnStartPause.Name = "btnStartPause";
+            this.btnStartPause.Size = new System.Drawing.Size(75, 23);
+            this.btnStartPause.TabIndex = 23;
+            this.btnStartPause.Text = "Start/Pause";
+            this.btnStartPause.UseVisualStyleBackColor = true;
+            this.btnStartPause.Click += new System.EventHandler(this.btnStartPause_Click);
+            // 
+            // textBoxIterations
+            // 
+            this.textBoxIterations.Enabled = false;
+            this.textBoxIterations.Location = new System.Drawing.Point(113, 381);
+            this.textBoxIterations.Name = "textBoxIterations";
+            this.textBoxIterations.Size = new System.Drawing.Size(100, 20);
+            this.textBoxIterations.TabIndex = 22;
+            // 
+            // textBoxStatus
+            // 
+            this.textBoxStatus.Enabled = false;
+            this.textBoxStatus.Location = new System.Drawing.Point(113, 314);
+            this.textBoxStatus.Name = "textBoxStatus";
+            this.textBoxStatus.Size = new System.Drawing.Size(100, 20);
+            this.textBoxStatus.TabIndex = 21;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(127, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(210, 31);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "My POMODORO";
             // 
             // lblfecha
             // 
             this.lblfecha.AutoSize = true;
             this.lblfecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblfecha.ForeColor = System.Drawing.SystemColors.MenuHighlight;
-            this.lblfecha.Location = new System.Drawing.Point(551, 58);
+            this.lblfecha.Location = new System.Drawing.Point(444, 46);
             this.lblfecha.Name = "lblfecha";
             this.lblfecha.Size = new System.Drawing.Size(64, 24);
             this.lblfecha.TabIndex = 18;
@@ -152,66 +174,25 @@
             this.lblhora.AutoSize = true;
             this.lblhora.Font = new System.Drawing.Font("Microsoft YaHei", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblhora.ForeColor = System.Drawing.SystemColors.MenuHighlight;
-            this.lblhora.Location = new System.Drawing.Point(584, 0);
+            this.lblhora.Location = new System.Drawing.Point(461, 0);
             this.lblhora.Name = "lblhora";
             this.lblhora.Size = new System.Drawing.Size(101, 46);
             this.lblhora.TabIndex = 17;
             this.lblhora.Text = "Hora";
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Sitka Text", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(163, 66);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(136, 28);
-            this.label1.TabIndex = 13;
-            this.label1.Text = "My Pomodoro";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(100, 66);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(57, 40);
-            this.pictureBox1.TabIndex = 12;
-            this.pictureBox1.TabStop = false;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(64, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(121, 31);
-            this.label2.TabIndex = 11;
-            this.label2.Text = "My Notes";
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(12, 9);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(46, 31);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 10;
-            this.pictureBox2.TabStop = false;
-            // 
             // timer1
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // pictureBox4
+            // timerPomodoro
             // 
-            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
-            this.pictureBox4.Location = new System.Drawing.Point(159, 73);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(205, 126);
-            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox4.TabIndex = 17;
-            this.pictureBox4.TabStop = false;
+            this.timerPomodoro.Tick += new System.EventHandler(this.timerPomodoro_Tick);
             // 
-            // Form2
+            // timermov
+            // 
+            this.timermov.Tick += new System.EventHandler(this.timermov_Tick);
+            // 
+            // frmTecnicaEstudio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -219,16 +200,13 @@
             this.Controls.Add(this.panelformularios);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(2);
-            this.Name = "Form2";
+            this.Name = "frmTecnicaEstudio";
             this.Text = "Form2";
             this.Load += new System.EventHandler(this.Form2_Load);
             this.panelformularios.ResumeLayout(false);
             this.panelformularios.PerformLayout();
-            this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -236,19 +214,19 @@
         #endregion
 
         private System.Windows.Forms.Panel panelformularios;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.Label lblhora;
         private System.Windows.Forms.Label lblfecha;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelTimer;
+        private System.Windows.Forms.Button btnEndSession;
+        private System.Windows.Forms.Button btnSkip;
+        private System.Windows.Forms.Button btnStartPause;
+        private System.Windows.Forms.TextBox textBoxIterations;
+        private System.Windows.Forms.TextBox textBoxStatus;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timerPomodoro;
+        private System.Windows.Forms.Timer timermov;
     }
 }
--- NOTES/ModernGUI_V3/frmTecnicaEstudio.cs
+++ NOTES/ModernGUI_V3/frmTecnicaEstudio.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,10 +15,118 @@ namespace ModernGUI_V3
 {
     public partial class frmTecnicaEstudio : Form
     {
+        private const int WorkMinutes = 25;
+        private const int RestMinutes = 5;
+
+        private int currentMinutes;
+        private int currentSeconds;
+
+        private int iterationCount = 0;
+
+        private bool isWorking = true;
+        private bool isPaused = false;
+
+        private readonly SoundPlayer notifySoundPlayer;
+        private SoundPlayer soundPlayer = new SoundPlayer();
+        private SoundPlayer soundPlayer2 = new SoundPlayer();
+
+        Bitmap bitmap = new Bitmap("C:\\Users\\50376\\Downloads\\NOTES\\ModernGUI_V3\\star.png"); // Bitmap
+        double angle = 0; // Ángulo inicial
+        int centerX; // Coordenada X del centro de rotación
+        int centerY; // Coordenada Y del centro de rotación
+        int radius = 100; // Radio de la órbita
         public frmTecnicaEstudio()
         {
            InitializeComponent();
+            timerPomodoro.Interval = 1000; // 1-second
+            timerPomodoro.Tick += timerPomodoro_Tick;
+            pictureBox2.Image = bitmap; // Mostrar la imagen en el PictureBox
+            centerX = pictureBox2.Width / 685; // Calcular el centro X del PictureBox
+            centerY = pictureBox2.Height / 480;
+
+            // sound path: PomodoroTimerApp\bin\Debug\net6.0-windows\sound.wav
+            string soundFileName = "C:\\Users\\50376\\Downloads\\NOTES\\ModernGUI_V3\\sound.wav";
+            notifySoundPlayer = InitializeSoundPlayer(soundFileName);
+
+            UpdateTimerDisplay();
         }
+        private void ShowErrorMessage(string message, string filePath)
+        {
+            MessageBox.Show($"{message}\n\nExpected path: {filePath}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private SoundPlayer InitializeSoundPlayer(string soundFileName)
+        {
+            try
+            {
+                if (File.Exists(soundFileName))
+                {
+                    return new SoundPlayer(soundFileName);
+                }
+
+                ShowErrorMessage("Sound file not found.", soundFileName);
+                return null;
+            }
+            catch (Exception ex)
+            {
+                ShowErrorMessage($"Error initializing sound player: {ex.Message}", soundFileName);
+                return null;
+            }
+        }
+        private void UpdateTimeRemaining()
+        {
+            if (currentSeconds == 0)
+            {
+                currentMinutes--;
+                currentSeconds = 59;
+            }
+            else
+            {
+                currentSeconds--;
+            }
+        }
+
+        private void PlayNotificationSound()
+        {
+            if (notifySoundPlayer != null)
+            {
+                try
+                {
+                    notifySoundPlayer.Play();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error playing sound: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void StartNewSession()
+        {
+            currentMinutes = WorkMinutes;
+            currentSeconds = 0;
+            iterationCount = 0;
+            isWorking = true;
+            isPaused = false;
+            timerPomodoro.Start();
+        }
+
+        private void SkipToNextPomodoro()
+        {
+            iterationCount++;
+            isWorking = !isWorking;
+            currentMinutes = isWorking ? WorkMinutes : RestMinutes;
+            currentSeconds = 0;
+        }
+
+        private void UpdateTimerDisplay()
+        {
+            labelTimer.Text = $"{currentMinutes:D2}:{currentSeconds:D2}";
+
+            textBoxStatus.Text = isWorking ? "Time to Focus!" : "Time for a Break!";
+            textBoxIterations.Text = $"Iteration: {iterationCount}";
+        }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -25,6 +135,10 @@ namespace ModernGUI_V3
 
         private void Form2_Load(object sender, EventArgs e)
         {
+            // Asignar la ruta del archivo de sonido al SoundPlayer
+            soundPlayer2.SoundLocation = "C:\\Users\\50376\\Downloads\\NOTES\\ModernGUI_V3\\ready.wav";
+            soundPlayer.SoundLocation = "C:\\Users\\50376\\Downloads\\NOTES\\ModernGUI_V3\\song.wav";
+
             timer1.Enabled = true;
         }
 
@@ -38,5 +152,83 @@ namespace ModernGUI_V3
         {
 
         }
+
+        private void timerPomodoro_Tick(object sender, EventArgs e)
+        {
+            if (currentMinutes == 0 && currentSeconds == 0)
+            {
+                isWorking = !isWorking;
+                iterationCount++;
+                currentMinutes = isWorking ? WorkMinutes : RestMinutes;
+                currentSeconds = 0;
+
+                PlayNotificationSound();
+            }
+            else
+            {
+                if (!isPaused)
+                {
+                    UpdateTimeRemaining();
+                }
+            }
+
+            UpdateTimerDisplay();
+        }
+
+        private void btnStartPause_Click(object sender, EventArgs e)
+        {
+            soundPlayer2.Play();
+            isPaused = !isPaused;
+
+            if (!timerPomodoro.Enabled)
+            {
+                StartNewSession();
+            }
+        }
+
+        private void btnSkip_Click(object sender, EventArgs e)
+        {
+            SkipToNextPomodoro();
+            PlayNotificationSound();
+            UpdateTimerDisplay();
+        }
+
+        private void btnEndSession_Click(object sender, EventArgs e)
+        {
+            // Reproducir el sonido al hacer clic en el botón
+            timermov.Start();
+            soundPlayer.Play();
+            timerPomodoro.Stop();
+            currentMinutes = 0;
+            currentSeconds = 0;
+            iterationCount = 0;
+            isWorking = true;
+            isPaused = false;
+            UpdateTimerDisplay();
+            pictureBox2.ImageLocation = "C:\\Users\\50376\\Downloads\\NOTES\\ModernGUI_V3\\star.jpg";
+
+
+            // Mover la imagen
+        }
+
+        private void timermov_Tick(object sender, EventArgs e)
+        {
+
+            int newX = (int)(685 + radius * Math.Cos(angle));
+            int newY = (int)(480 + radius * Math.Sin(angle));
+
+            // Establecer la nueva ubicación de la imagen
+            pictureBox2.Location = new Point(newX, newY);
+
+            // Actualizar el ángulo para el próximo paso
+            angle += 0.05;
+        }
+
+        private void btnEndSession_MouseClick(object sender, MouseEventArgs e)
+        {
+            pictureBox2.Visible = true;
+        }
+
+      
     }
 }
